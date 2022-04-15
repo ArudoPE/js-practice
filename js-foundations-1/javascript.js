e7cfacc5526736c0d3e551c5a07ff1eefd625865
ArudoPE <aortizparodi@outlook.com>
2022-04-14 19:12:00 -0500

    Add first lesson of foundations

---
+++ js-foundations-1/javascript.js
@@ -0,0 +1,48 @@
+//console.log("Bye World!")
+
+//Declaring a variable
+//let message;
+
+//message = 'Hello';
+
+//alert(message);
+
+//We can also declare and assign in a single line
+
+// let message = 'Hello!';
+
+// alert(message);
+
+//We can also declare multiple variables in a single line (but nor recommended)
+
+//let user = 'Jhon', age = 25, message = 'Hello';
+
+//let user = 'Jhon';
+//let age = 25;
+//let message = 'Hello';
+
+//We can replace the value of a variable
+
+//let message;
+
+//message = 'Hello';
+
+//message = 'World!';
+
+//alert(message);
+
+//We can also declare and copy a variable content
+
+//let hello = 'Hello World!';
+
+//let message;
+
+//message = hello;
+
+//alert(hello);
+//alert(message);
+
+//We can't declare a let variable twice
+//let message = 'This';
+
+//let message = 'That';

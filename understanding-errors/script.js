5cb89cdfc0f88f29c6434d90a9fc79705f446e89
ArudoPE <aortizparodi@outlook.com>
2022-05-30 23:12:14 -0500

    Starting 'Understanding Errors' lesson

---
+++ understanding-errors/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//example1: ReferenceError
+// const a = "Hello";
+
+// const b = "World";
+
+// console.log(c);
+
+//example2:
+
+const a = 5;
+const b = 10;
+
+const add = () => {
+    return c;
+}
+
+const print = () => {
+    add();
+}
+
+print();
